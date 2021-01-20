462225b85011fcf784a07bf17f57965ad77ab885
Evgenia Bazhaikina <bbevgenia@yandex.ru>
2021-01-20 11:36:45 +0700

    МС-1 Настроен роутинг

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import MainPage from '../views/MainPage.vue';
+import CardsView from '../views/CardsView.vue';
+import CardsForward from '../views/CardsForward.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'MainPage',
+    component: MainPage,
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'CardsView',
+    component: CardsView,
+  },
+  {
+    path: '/forward',
+    name: 'CardsForward',
+    component: CardsForward,
   },
 ];
 
